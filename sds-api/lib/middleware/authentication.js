c37e7f6e8961ee2456cfbe83db49a7c3faca7519
Buford Eeds <bufordeeds8@gmail.com>
2024-01-30 13:36:35 -0600

    turning on auth so we can pet them dogs

--- sds-api/lib/middleware/authentication.js
+++ sds-api/lib/middleware/authentication.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const publicKey = fs.readFileSync('./keys/rsa_public.key');
 
-let turn_off = true;
+let turn_off = false;
 // turn_off = true;
 
 if (process.env.NODE_ENV === 'production') {

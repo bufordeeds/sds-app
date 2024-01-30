a478e2506a75bbdb19f55f2c28152affff3b3fb4
Buford Eeds <bufordeeds8@gmail.com>
2024-01-30 14:55:28 -0600

    add storybook config

---
+++ sds/.storybook/main.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/vue-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
--- sds/src/components/app/avatar.vue
+++ sds/src/components/app/avatar.vue
@@ -1,44 +1,28 @@
 <template>
-  <div
-    :style="container_style"
-    @click="$emit('click')"
-  >
-    <v-avatar
-      :size="size2"
-      :tile="!rounded"
-      :style="avatar_style"
-    >
-      <img
-        v-if="!image2.Location"
-        :src="no_image"
-      >
-      <img
-        v-else
-        :src="image2.Location"
-      >
-    </v-avatar>
+   <div :style="container_style" @click="$emit('click')">
+      <v-avatar :size="size2" :tile="!rounded" :style="avatar_style">
+         <img v-if="!image2.Location" :src="no_image">
+         <img v-else :src="image2.Location">
+      </v-avatar>
 
-    <div
-      v-if="profile && !imageOnly"
-      class="ml-3"
-    >
-      <div v-if="profile.name">
-        {{ profile.name }}
-      </div>
+      <div v-if="profile && !imageOnly" class="ml-3">
+         <div v-if="profile.name">
+            {{ profile.name }}
+         </div>
 
-      <div v-if="profile.name_first || profile.name_last">
-        {{ profile.name_first }} {{ profile.name_last }}
-      </div>
+         <div v-if="profile.name_first || profile.name_last">
+            {{ profile.name_first }} {{ profile.name_last }}
+         </div>
 
-      <div v-if="profile.email">
-        {{ profile.email }}
-      </div>
+         <div v-if="profile.email">
+            {{ profile.email }}
+         </div>
 
-      <div v-if="profile.dog_num">
-        {{ profile.dog_num }}
+         <div v-if="profile.dog_num">
+            {{ profile.dog_num }}
+         </div>
       </div>
-    </div>
-  </div>
+   </div>
 </template>
 
 <script>
@@ -46,57 +30,57 @@ export default {
    name: "Avatar",
    props: {
       // image: {type: Object, default: () => { return {} }}, // s3/spaces storage object
-      image: {type: Object, default: null}, // s3/spaces storage object
-      profile: {type: Object, default: null},
+      image: { type: Object, default: null }, // s3/spaces storage object
+      profile: { type: Object, default: null },
 
-      borderWidth: {type: String, default: null},
-      borderColor: {type: String, default: 'white'},
-      size: {type: String, default: "100"},
-      sizeNoImage: {type: String, default: null},
-      rounded: {type: Boolean, default: true},
-      profileType:  {type: String, default: 'user'},
-      imageOnly: {type: Boolean, default: false}, //if true only display image
+      borderWidth: { type: String, default: null },
+      borderColor: { type: String, default: 'white' },
+      size: { type: String, default: "100" },
+      sizeNoImage: { type: String, default: null },
+      rounded: { type: Boolean, default: true },
+      profileType: { type: String, default: 'user' },
+      imageOnly: { type: Boolean, default: false }, //if true only display image
    },
 
-   computed:{
+   computed: {
 
-      container_style(){
-         if (this.imageOnly){
+      container_style() {
+         if (this.imageOnly) {
             return "";
          }
-         else{
+         else {
             return "display: flex; justify-content: center"
          }
       },
 
-     avatar_style(){
-       let ans = '';
-       if (this.borderWidth !== null){
-         ans = `border: ${this.borderWidth} solid ${this.borderColor};`;
-         // if (this.rounded){
-         //   ans += `border-radius: 50%;`;
-         // }
+      avatar_style() {
+         let ans = '';
+         if (this.borderWidth !== null) {
+            ans = `border: ${this.borderWidth} solid ${this.borderColor};`;
+            // if (this.rounded){
+            //   ans += `border-radius: 50%;`;
+            // }
 
-       }
+         }
 
-       return ans;
+         return;
 
-     },
+      },
 
 
-      size2(){
+      size2() {
 
-         if (this.image2.Location == undefined){
-            if (this.sizeNoImage !== null){
+         if (this.image2.Location == undefined) {
+            if (this.sizeNoImage !== null) {
                return this.sizeNoImage;
             }
-            else{
+            else {
                return this.size;
             }
          }
 
 
-         else{
+         else {
             return this.size;
          }
 
@@ -104,24 +88,24 @@ export default {
       },
 
 
-      no_image(){
-        if (this.profileType === 'user'){
-           return require('../../assets/images/content/user-no-image.png')
-        }
-        else if (this.profileType === 'dog'){
-           return require('../../assets/images/content/dog-no-image.png')
-        }
-        return null;
+      no_image() {
+         if (this.profileType === 'user') {
+            return require('../../assets/images/content/user-no-image.png')
+         }
+         else if (this.profileType === 'dog') {
+            return require('../../assets/images/content/dog-no-image.png')
+         }
+         return null;
       },
 
-      image2(){
+      image2() {
          // console.log('debug')
 
-         if (this.image != null){
+         if (this.image != null) {
             return this.image;
          }
-         else{
-            if (this.profile !== null && this.profile.profile_image){
+         else {
+            if (this.profile !== null && this.profile.profile_image) {
                return this.profile.profile_image;
             }
             return {};
@@ -131,6 +115,4 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

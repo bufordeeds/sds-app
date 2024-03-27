09a58c4f430f405151bff0e29236a0ffe9accf65
Buford Eeds <bufordeeds8@gmail.com>
2024-03-27 11:38:49 -0500

    update primary color

--- sds/src/plugins/vuetify.js
+++ sds/src/plugins/vuetify.js
@@ -4,25 +4,25 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-   theme: {
-      dark: false,
-      light: true,
-      themes: {
-         light: {
-            primary: '#f1f',
-            secondary: '#000000',
-            accent: '#7fff57',
-            error: '#ff0e1d',
-         },
+	theme: {
+		dark: false,
+		light: true,
+		themes: {
+			light: {
+				primary: '#0066DF',
+				secondary: '#000000',
+				accent: '#7fff57',
+				error: '#ff0e1d',
+			},
 
-         dark: {
-            primary: '#00a3fc',
-            secondary: '#b0bec5',
-            accent: '#8c9eff',
-            error: '#ff1a06',
-         },
-      },
-   },
-})
+			dark: {
+				primary: '#00a3fc',
+				secondary: '#b0bec5',
+				accent: '#8c9eff',
+				error: '#ff1a06'
+			}
+		}
+	}
+});
 
 

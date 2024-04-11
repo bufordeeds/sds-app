ceeae68a2f2ad88733db4eac805956f12160da8d
Buford Eeds <bufordeeds8@gmail.com>
2024-04-11 13:16:09 -0500

    add sort icons and update sort icon component with new icons.

--- sds/src/components/icons/SortIcon.vue
+++ sds/src/components/icons/SortIcon.vue
@@ -1,104 +1,58 @@
 <template>
-  <v-btn
-    icon
-    x-small
-    :color="icon_color"
-    @click="on_click"
-  >
-    <v-icon small>
-      {{ icon_dir }}
-    </v-icon>
-  </v-btn>
+   <v-btn icon x-small :color="icon_color" @click="on_click">
+      <img v-if="sortDir2 === 'ascending'" src="../../assets/images/icons/sort-ascending.svg" alt="Ascending"
+         style="width: 18px; height: 18px;" />
+      <img v-else-if="sortDir2 === 'descending'" src="../../assets/images/icons/sort-descending.svg" alt="Descending"
+         style="width: 18px; height: 18px;" />
+      <img v-else src="../../assets/images/icons/sort-unselected.svg" alt="Unselected"
+         style="width: 18px; height: 18px;" />
+   </v-btn>
 </template>
 
 <script>
-
-
-/*
-   note ascending means first array item is the smallest, last item is the largest, i.e. [1, 2, 4 ], ['a', 'b', 'c']
-
-   up arrow = descending
- */
 export default {
    name: "SortIcon",
    props: {
-      sortDir: {type: String, default: 'descending'}, // ascending | descending | none.
-
-      disabled:  {type: Boolean, default: false}, // controls whether the associated list is sorted at all
-
+      sortDir: { type: String, default: 'descending' }, // ascending | descending | none.
+      disabled: { type: Boolean, default: false }, // controls whether the associated list is sorted at all
    },
-   data(){
+   data() {
       return {
          sortDir2: this.sortDir,
-      }
+      };
    },
    computed: {
-      icon_dir(){
-         if (this.sortDir2 === 'descending' ){
-            return 'north';
-         }
-         else if (this.sortDir2 === 'ascending' ){
-            return 'south';
-         }
-         else{
-            return 'north';
-         }
+      icon_color() {
+         return this.sortDir2 === 'none' ? '#b1b1b1' : 'var(--color-btn)';
       },
-
-     icon_color(){
-        let ans = 'var(--color-btn)';
-        if (this.sortDir2 === 'none'){
-           ans = '#b1b1b1'
-        }
-
-        return ans;
-     }
    },
-
-   watch:{
-      // active(newVal){
-      //    if (this.active2 !== newVal){
-      //       this.active2 = newVal;
-      //    }
-      // },
-      //
-      // active2(newVal){
-      //    this.$emit('update:active', newVal);
-      // },
-
-      sortDir(newVal){
-         if (this.sortDir2 !== newVal){
+   watch: {
+      sortDir(newVal) {
+         if (this.sortDir2 !== newVal) {
             this.sortDir2 = newVal;
          }
       },
-
-      sortDir2(newVal){
+      sortDir2(newVal) {
          this.$emit('update:sortDir', newVal);
       },
-
-
    },
-
-   methods:{
-      on_click(){
-         if (this.sortDir2 === 'none'){
-            this.sortDir2 = 'ascending'
+   methods: {
+      on_click() {
+         if (this.sortDir2 === 'none') {
+            this.sortDir2 = 'ascending';
+         } else if (this.sortDir2 === 'ascending') {
+            this.sortDir2 = 'descending';
+         } else if (this.sortDir2 === 'descending') {
+            this.sortDir2 = 'ascending';
          }
-
-         else if (this.sortDir2 === 'ascending'){
-            this.sortDir2 = 'descending'
-         }
-
-         else if (this.sortDir2 === 'descending'){
-            this.sortDir2 = 'ascending'
-         }
-
          this.$emit('click', this.sortDir2);
-      }
-   }
-}
+      },
+   },
+};
 </script>
 
 <style scoped>
-
+.theme--light.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+   background-color: transparent !important;
+}
 </style>

a13c51138a652fb0cd45a46cf7de8071bfc6997d
Adam Age <adamwage@hotmail.com>
2024-10-08 18:44:42 -0500

    generic modal component

---
+++ sds/src/components/app/modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="modal">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+}
+</script>
+
+<style scoped lang="scss">
+  @import url('../../views/vars.css');
+
+  .modal {
+    align-items: center;
+    background-color: rgba(193, 201, 210, 0.7);
+    backdrop-filter: blur(10px);
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    left: 0;
+    position: fixed;
+    right: 0;
+    top: 0;
+  }
+</style>

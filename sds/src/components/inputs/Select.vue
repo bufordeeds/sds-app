ad9b23e91555ae7a1c9a517c8d2859479dbbad9d
Adam Age <adamwage@hotmail.com>
2024-10-21 18:08:45 -0700

    single select

---
+++ sds/src/components/inputs/Select.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    id="my-select__container"
+    class="flex flex-col"
+  >
+    <label :for="id">{{ label }}</label>
+    <select
+      :id="id"
+      :name="name"
+    >
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MySelect",
+  inject: ['registerThisField', 'unRegisterField'],
+  props: {
+    id: { default: "my_select", required: true, type: String },
+    label: { default: "Default Label", required: true, type: String },
+    name: { default: "my_select_name", type: String },
+    options: { default: () => [], required: true, type: Array },
+    rules: { default: () => [], type: Array },
+  },
+  created(){
+    if (this.registerThisField !=  null){
+      this.field_id = this.registerThisField(this);
+    }
+  },
+  beforeDestroy() {
+      this.unRegisterField(this.field_id);
+  },
+  methods: {
+    validate() {
+        this.errorMessage = null;
+
+        for (let f of this.rules) {
+          let ans = f(this.inputVal);
+
+          if (ans !== true) {
+              this.errorMessage = ans.toString();
+              console.log('Validation ran', ans)
+              return false;
+          }
+        }
+
+        return true;
+    },
+  }
+}
+</script>
+
+<style lang='scss'>
+@import url('../../views/vars.css');
+
+#my-select__container {
+  position: relative;
+  row-gap: 8px;
+
+  select {
+    appearance: none;
+    background-color: var(--surface-light-white);
+    border: 1px solid var(--border-default);
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 17px;
+    height: 44px;
+    line-height: 24px;
+    padding: 0 12px;
+  }
+
+  select::-ms-expand {
+    display: none;
+  }
+
+  &::after {
+    border-bottom: 2px solid var(--border-default);
+    border-left: 2px solid var(--border-default);
+    bottom: 18px;
+    content: '';
+    display: inline-block;
+    height: 10px;
+    position: absolute;
+    right: 18px;
+    transform: rotate(-45deg);
+    width: 10px;
+  }
+}
+</style>

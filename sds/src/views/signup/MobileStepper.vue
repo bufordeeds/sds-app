1c533ae335d0d826230c9ad26e188fdedaf05cbb
Adam Age <adamwage@hotmail.com>
2024-11-09 18:35:03 -0600

    switching from scss to css since scss seems to be broken

--- sds/src/views/signup/MobileStepper.vue
+++ sds/src/views/signup/MobileStepper.vue
@@ -10,8 +10,8 @@
       >
         <span class="steps__counter">1</span>
         <span class="flex flex-col">
-          <p>Step 1/4</p>
-          <p>
+          <p class="step__info">Step 1 / 4</p>
+          <p class="step__label">
             Account Type
           </p>
         </span>
@@ -22,8 +22,8 @@
       >
         <span class="steps__counter">2</span>
         <span class="flex flex-col">
-          <p>Step 2/4</p>
-          <p>
+          <p class="step__info">Step 2 / 4</p>
+          <p class="step__label">
             Email Verification
           </p>
         </span>
@@ -34,8 +34,8 @@
       >
         <span class="steps__counter">3</span>
         <span class="flex flex-col">
-          <p>Step 3/4</p>
-          <p>
+          <p class="step__info">Step 3 / 4</p>
+          <p class="step__label">
             Terms & Conditions
           </p>
         </span>
@@ -46,8 +46,8 @@
       >
         <span class="steps__counter">4</span>
         <span class="flex flex-col">
-          <p>Step 4/4</p>
-          <p>Basic Info</p>
+          <p class="step__info">Step 4 / 4</p>
+          <p class="step__label">Basic Info</p>
         </span>
       </li>
     </ol>
@@ -76,74 +76,88 @@ export default {
 <style scoped lang="scss">
 @import url('../main.css');
 
+#mobile-stepper {
+  list-style-type: none;
+  margin: 0;
+  padding: 16px 16px 0 16px;
+  row-gap: 16px;
+}
+
+.steps__list-item {
+  color: var(--text-default);
+  font-size: 17px;
+  font-weight: 700;
+  line-height: 24px;
+  text-align: left;
+}
+
+p,
+ol {
+  margin: 0;
+  padding: 0;
+}
+    
+.steps__counter {
+  align-items: center;
+  background-color: var(--surface-light-white);
+  border-radius: 50%;
+  color: var(--text-medium);
+  display: flex;
+  flex-grow: 0;
+  font-weight: 700;
+  height: 40px;
+  justify-content: center;
+  position: relative;
+  width: 40px;
+}
+
+.step__info {
+  color: var(--text-medium, #616165);
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 18px;
+  letter-spacing: 0.20000000298023224px;
+  text-align: left;
+}
+
+.step__label {
+  color: var(--text-default, #2F2F31);
+  font-size: 17px;
+  font-weight: 700;
+  line-height: 24px;
+  text-align: left;
+}
+
+.steps__list-item.steps__list-item--active  {
+  color: var(--brand-primary-500);
+}
+
+.steps__list-item.steps__list-item--active .steps__counter {
+  background-color: var(--button-form-button);
+  color: var(--text-white);
+}
+
+.steps__list-item.steps__list-item--finished .steps__counter::before {
+  background-color: var(--surface-light-white);
+  border: 2px solid var(--button-form-button);
+  border-radius: 50%;
+  content: '';
+  height: 40px;
+  position: absolute;
+  width: 40px;
+}
+
+.steps__list-item.steps__list-item--finished .steps__counter::after {
+  border-bottom: 2px solid var(--button-form-button);
+  border-right: 2px solid var(--button-form-button);
+  content: '';
+  height: 20px;
+  transform: rotate(45deg) translate(-5px, 1px);
+  width: 10px;
+}
+@media only screen and (min-width: 768px) {
   #mobile-stepper {
-    list-style-type: none;
-    margin: 0;
-    padding: 16px 16px 0 16px;
-    row-gap: 16px;
-
-    .steps__list-item {
-      color: var(--text-default);
-      font-size: 17px;
-      font-weight: 700;
-      line-height: 24px;
-      text-align: left;
-
-      p,
-      ol {
-        margin: 0;
-        padding: 0;
-      }
-      
-      .steps__counter {
-        align-items: center;
-        background-color: var(--surface-light-white);
-        border-radius: 50%;
-        color: var(--text-medium);
-        display: flex;
-        flex-grow: 0;
-        font-weight: 700;
-        height: 40px;
-        justify-content: center;
-        position: relative;
-        width: 40px;
-      }
-      
-      &.steps__list-item--active  {
-        color: var(--brand-primary-500);
-
-        .steps__counter {
-          background-color: var(--button-form-button);
-          color: var(--text-white);
-        }
-      }
-
-      &.steps__list-item--finished {
-        .steps__counter::before {
-          background-color: var(--surface-light-white);
-          border: 2px solid var(--button-form-button);
-          border-radius: 50%;
-          content: '';
-          height: 40px;
-          position: absolute;
-          width: 40px;
-        }
-
-        .steps__counter::after {
-          border-bottom: 2px solid var(--button-form-button);
-          border-right: 2px solid var(--button-form-button);
-          content: '';
-          height: 20px;
-          transform: rotate(45deg) translate(-5px, 1px);
-          width: 10px;
-        }
-      }
-    }
+    display: none;
   }
-
-  @media only screen and (min-width: 768px) {
-    #mobile-stepper {
-      display: none;
-    }
-  }
+}
 </style>
--- sds/src/views/signup/Stepper.vue
+++ sds/src/views/signup/Stepper.vue
@@ -124,200 +124,67 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-  #steps {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    row-gap: 32px;
-
-    .steps__list-item {
-      font-size: var(--rt-h5);
-      font-weight: 600;
-      color: var(--text-medium);
-      
-      .steps__counter {
-        align-items: center;
-        background-color: var(--surface-light-white);
-        border-radius: 50%;
-        color: var(--text-medium);
-        
-        .steps__counter {
-          align-items: center;
-          background-color: var(--surface-light-white);
-          border-radius: 50%;
-          color: var(--text-medium);
-          display: flex;
-          flex-grow: 0;
-          font-weight: 700;
-          height: 40px;
-          justify-content: center;
-          position: relative;
-          width: 40px;
-        }
-        
-        &.steps__list-item--active  {
-          color: var(--brand-primary-500);
-
-          .steps__counter {
-            background-color: var(--button-form-button);
-            color: var(--text-white);
-          }
-        }
-
-        &.steps__list-item--finished {
-          .steps__counter::before {
-            background-color: var(--surface-light-white);
-            border: 2px solid var(--button-form-button);
-            border-radius: 50%;
-            content: '';
-            height: 40px;
-            position: absolute;
-            width: 40px;
-          }
-
-          .steps__counter::after {
-            border-bottom: 2px solid var(--button-form-button);
-            border-right: 2px solid var(--button-form-button);
-            content: '';
-            height: 20px;
-            transform: rotate(45deg) translate(-5px, 1px);
-            width: 10px;
-          }
-        }
-      }
-    }
-  }
-
-/**
-*
-*
-* 10/04 Removed old Stepper styles aage
-*
-*
-*/
-
-/* .breadcrumb-container{
-    display: flex;
+<style>
+#steps {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  row-gap: 32px;
 }
 
-.block{
-    background-color: #61afe1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 40px;
-    padding-left: 20px;
-    padding-right: 10px;
-    margin-left: -10px;
-    color: white;
-    font-size: 13pt;
-    font-weight: 500;
+.steps__list-item {
+  font-size: var(--rt-h5);
+  font-weight: 600;
+  color: var(--text-medium);
+}
+
+.steps__list-item.steps__list-item--active  {
+  color: var(--brand-primary-500);
+}
+
+.steps__counter {
+  align-items: center;
+  background-color: var(--surface-light-white);
+  border-radius: 50%;
+  color: var(--text-medium);
+}
+
+.steps__list-item.steps__list-item--active .steps__counter {
+  background-color: var(--button-form-button);
+  color: var(--text-white);
+}
+
+.steps__list-item .steps__counter {
+  align-items: center;
+  background-color: var(--surface-light-white);
+  border-radius: 50%;
+  color: var(--text-medium);
+  display: flex;
+  flex-grow: 0;
+  font-weight: 700;
+  height: 40px;
+  justify-content: center;
+  position: relative;
+  width: 40px;
 }
 
 
-.svg-container{
-    width: 10px;
-    height: 40px;
-    z-index: 2;
+.steps__list-item.steps__list-item--finished .steps__counter::before {
+  background-color: var(--surface-light-white);
+  border: 2px solid var(--button-form-button);
+  border-radius: 50%;
+  content: '';
+  height: 40px;
+  position: absolute;
+  width: 40px;
 }
-.svg-triangle{
-    fill: #61afe1;
-    stroke: #dedede;
-    stroke-width:2;
+
+.steps__list-item.steps__list-item--finished .steps__counter::after {
+  border-bottom: 2px solid var(--button-form-button);
+  border-right: 2px solid var(--button-form-button);
+  content: '';
+  height: 20px;
+  transform: rotate(45deg) translate(-5px, 1px);
+  width: 10px;
 }
-
-@media screen and (max-width: 500px) {
-    .block{
-        font-size: 11pt;
-    }
-} */
-
-/*!*get rid of the normal styling for a list*!*/
-/*.breadcrumb {*/
-/*   list-style: none;*/
-/*   overflow: hidden;*/
-/*   font: 18px Sans-Serif;*/
-/*   padding-left: 0px;*/
-/*   padding-right: 32px*/
-/*}*/
-/*.breadcrumb li {*/
-/*   float: left;*/
-/*}*/
-
-/*!*style the boxes  as we like*!*/
-/*.breadcrumb li a {*/
-/*   color: white;*/
-/*   text-decoration: none;*/
-/*   padding: 10px 0 10px 65px;*/
-/*   !*background: brown; !* fallback color *!*!*/
-/*   background: #61afe1;*/
-/*   position: relative;*/
-/*   display: block;*/
-/*   float: left;*/
-/*}*/
-
-/*!*creates triangle at end of each li element *!*/
-/*.breadcrumb li a::after {*/
-/*   content: " ";*/
-/*   display: block;*/
-/*   width: 0;*/
-/*   height: 0;*/
-/*   border-top: 50px solid transparent; !* Go big on the size, and let overflow hide *!*/
-/*   border-bottom: 50px solid transparent;*/
-/*   border-left: 30px solid #61afe1;*/
-/*   position: absolute;*/
-/*   top: 50%;*/
-/*   margin-top: -50px;*/
-/*   left: 100%;*/
-/*   z-index: 2;*/
-
-/*}*/
-
-
-/*!*creates another triangle that overlays to create a white border effect*!*/
-/*.breadcrumb li a:before {*/
-/*   content: " ";*/
-/*   display: block;*/
-/*   width: 0;*/
-/*   height: 0;*/
-/*   border-top: 50px solid transparent;           !* Go big on the size, and let overflow hide *!*/
-/*   border-bottom: 50px solid transparent;*/
-/*   border-left: 30px solid #dbdbdb;*/
-/*   position: absolute;*/
-/*   top: 50%;*/
-/*   margin-top: -50px;*/
-/*   margin-left: 2px;*/
-/*   left: 100%;*/
-/*   z-index: 1;*/
-/*}*/
-
-
-/*.breadcrumb li:first-child a {*/
-/*   padding-left: 10px;*/
-/*}*/
-
-
-
-
-/*!*controls color for highlighted section*!*/
-/*li.highlighted a {*/
-/*   background: #8cc63f !important;*/
-/*}*/
-/*li.highlighted a:after {*/
-/*   border-left-color: #8cc63f !important;*/
-/*}*/
-
-
-/*!*controls color for finished section*!*/
-/*li.finished a {*/
-/*   background: #658d2f !important;*/
-/*}*/
-/*li.finished a:after {*/
-/*   border-left-color: #658d2f !important;*/
-/*}*/
-
-
-
-
 </style>
